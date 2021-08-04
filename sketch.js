cf7224e237e8e68b3bc718365368177bb53a47ea
nimbesh23 <88438453+nimbesh23@users.noreply.github.com>
2021-08-04 17:53:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var box ;
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200,200,20,100);
+  box.shapeColor = "blue";
+}
+
+function draw() 
+{
+  background(255);
+  drawSprites();
+  if(keyIsDown (RIGHT_ARROW)) {
+    box.x = box.x+5;
+  }
+  if(keyIsDown (LEFT_ARROW)) {
+    box.x = box.x-5;
+  }
+  if(keyIsDown (UP_ARROW)) {
+    box.y = box.y-5;
+  }
+  if(keyIsDown (DOWN_ARROW)) {
+    box.y = box.y+5;
+  }
+}
+
+
+
